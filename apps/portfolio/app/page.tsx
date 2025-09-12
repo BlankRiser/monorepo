3db48650f7afeb9ea534dbde59fb01d9864e4da8
ram <rschoudhary1999@gmail.com>
2025-09-12 17:02:07 +0530

    feat: add required ui components

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@workspace/ui/components/button"
+import { Badge } from "@workspace/ui/components/badge";
+import { Button } from "@workspace/ui/components/button";
+import { cn } from "@workspace/ui/lib/utils";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className={cn(["text-2xl font-bold"])}>Hello World</h1>
+        <Button size="sm" variant="ghost">
+          Button
+        </Button>
+        <Badge variant={"secondary"}>nice</Badge>
       </div>
     </div>
-  )
+  );
 }
---
+++ packages/ui/src/lib/themes.ts
@@ -0,0 +1,12 @@
+export const THEMES = [
+  {
+    theme: "retro-arcade",
+    label: "Retro Arcade",
+    colors: [
+        "oklch(0.7227 0.192 149.5793)",
+        "oklch(0.2654 0.0651 152.7696)",
+        "oklch(0.2664 0.0628 152.9343)",
+        "oklch(0.2671 0.0177 141.6456)"
+    ]
+  },
+];
